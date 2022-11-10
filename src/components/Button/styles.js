a120dccd50029b44ff9d23e84268ff5b495ab33e
Vitor-Kemuel <kemuvitor@gmail.com>
2022-11-09 21:10:46 -0300

    Fazendo designer e funcionalidades do button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,1 +1,20 @@
 import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+
+    background: #00AAF0;
+    color: #FFF;
+    font-size: 24px;
+    font-weight: 700;
+
+    flex: 1;
+    border-radius: 10px;
+    margin: 2px;
+
+    &:hover{
+        opacity: 0.6;
+        cursor: pointer;
+    }
+`
